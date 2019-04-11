668fd89082235c292a62d8c1df4e6ff66e8fa083
ForSureNotKobe <j.porebski0@gmail.com>
2019-04-12 00:27:59 +0200

    update

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -4,41 +4,6 @@ import java.util.*;
 import java.lang.Double;
 
 public class main {
-    public static void main(String[] args) throws FileNotFoundException {
-        List<Products> ListOfAll = new ArrayList<Products>();
-        List<Bag> bags = new ArrayList<Bag>();
-        Bag bag = new Bag();
-        int BagLoad = bag.getBagLoad();
-
-        double dbweight;
-        Scanner scanner = new Scanner(new File("/Users/jpore/Data/product_data.csv"));
-        scanner.useDelimiter(",");
-        scanner.nextLine();                          //skipping first line of headears
-        while (scanner.hasNext()) {
-            String id = scanner.next();
-            if (id.matches("[0-9]+")) {
-            } else {
-                scanner.nextLine();
-                continue;
-            }
-            String name = scanner.next();
-            String weight = scanner.next();
-            String unit = scanner.nextLine();
-            dbweight = Double.parseDouble(weight);
-            if (unit.equals(",KG")) {
-                dbweight = dbweight * 1000.0;
-            }
-            int intweight = (int) dbweight;
-            ListOfAll.add(new Products(Integer.parseInt(id), name, intweight));
-        }
-        scanner.close();
-
-        List<Long> productsID = new ArrayList<>();
-        productsID.add(73323011L);
-        productsID.add(77760011L);
-        productsID.add(94683011L);
-        productsID.add(56966011L);
-        productsID.add(241566011L);
     }
 
     public void BagCounter(List<Long> productsID) {
